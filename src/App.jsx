37d1d120818c4130796e8265a4ac017c939bbc2d
Neil Kandalgaonkar <neilk@brevity.org>
2017-04-22 19:36:19 -0700

    flipped hist

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import TextDisplay from './components/TextDisplay';
 // console.log(githistory);
 
 
-const hist = [{
+const hist = [
+{
   "commit": "a9894a8194159ad9963565ade75038a7635cfeb2",
   "text": "FROM nginx:1.9.10\nMAINTAINER Bench <dev@bench.co>\n\nCOPY ./target /app/\nCOPY ./.nginx-default.conf /etc/nginx/conf.d/default.conf\n\nEXPOSE 8079\nCMD nginx -g 'daemon off;'\n",
   "author": "Trevor O <trevor@trevoro.net>",
@@ -23,6 +24,13 @@ const hist = [{
 },
 {
   "commit": "d24941dd1a05112c157db6313d54a6e7aeab1561",
+  "text": "FROM node:0.12.9\nMAINTAINER Bench <dev@bench.co>\n\nRUN echo '{ \"allow_root\": true }' > /root/.bowerrc\nRUN echo 'unsafe-perm = true' > /root/.npmrc\n\nCOPY package.json bower.json /app/\nWORKDIR /app\nRUN npm install --quiet\n\nCOPY . /app\nWORKDIR /app\nRUN npm run build\nRUN chmod +x /app/docker_entry.sh\n\nEXPOSE 80 8079\n\nENV RIND_ENV benchception\n\nENTRYPOINT /app/docker_entry.sh\n\n",
+  "author": "Jean-Martin Archer <jm@bench.co>",
+  "date": "Fri Jan 29 10:08:42 2016 -0800",
+  "message": "adding entry point"
+},
+{
+  "commit": "d24941dd1a05112c157db6313d54a6e7aeab1561",
   "text": "FROM node:0.12.9\nMAINTAINER Bench <dev@bench.co>\n\n# allow us to run npm and bower as root\nRUN echo '{ \"allow_root\": true }' > /root/.bowerrc\nRUN echo 'unsafe-perm = true' > /root/.npmrc\n\n# caches npm install as a layer for faster builds\nCOPY package.json bower.json /app/\nWORKDIR /app\nRUN npm install --quiet\n\nCOPY . /app\nWORKDIR /app\nRUN npm run build\nRUN chmod +x /app/docker_entry.sh\n\nEXPOSE 80 8079\n\nENV RIND_ENV benchception\n\nENTRYPOINT /app/docker_entry.sh\n\n",
   "author": "Jean-Martin Archer <jm@bench.co>",
   "date": "Fri Jan 29 10:08:42 2016 -0800",
@@ -34,6 +42,13 @@ const hist = [{
   "author": "Trevor O <trevor@trevoro.net>",
   "date": "Thu Jan 28 17:47:41 2016 -0800",
   "message": "add docker build mechanism fixes 595"
+},
+{
+  "commit": "a5495b4ec20085bbf094cc2ac34d0a5495b4ec2",
+  "text": "FROM node:0.12.9\n\n\n\n# Hi this is Neil\nIsn't this cool?\n\n\MAINTAINER Bench <dev@bench.co>\n\n# allow us to run npm and bower as root\nRUN echo '{ \"allow_root\": true }' > /root/.bowerrc\nRUN echo 'unsafe-perm = true' > /root/.npmrc\n\n# caches npm install as a layer for faster builds\nCOPY package.json bower.json /app/\nWORKDIR /app\nRUN npm install --quiet\n\nCOPY . /app\nWORKDIR /app\nRUN npm run build\n\nEXPOSE 80\n",
+  "author": "Neil Kandalgaonkar <neilk@brevity.org>",
+  "date": "Thu Apr 21 17:47:41 2016 -0800",
+  "message": "Hackathon!!"
 }];
 
 let i = 0;
